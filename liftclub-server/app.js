61763ace654af1f8078fd40476bcc7923ed1d8db
cameron-dryden <cameronjames.dryden@gmail.com>
2022-11-29 07:56:34 +0200

    Testing

--- liftclub-server/app.js
+++ liftclub-server/app.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 const Liftclub = require("./models/liftclub");
 const User = require("./models/user");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/liftclubApp", { useNewUrlParser: true })
   .then(() => {
@@ -22,6 +25,7 @@ app.get("/r/:id", (req, res) => {
 
 app.get("/liftclubs", async (req, res) => {
   const liftclubs = await Liftclub.find({});
+  console.log(liftclubs);
   res.send(
     `Liftclub 1: ${liftclubs[0]._id} - ${liftclubs[0].name}, Liftclub 2: ${liftclubs[1]._id} - ${liftclubs[1].name}`
   );
@@ -29,6 +33,7 @@ app.get("/liftclubs", async (req, res) => {
 
 app.get("/users", async (req, res) => {
   const users = await User.find({});
+  console.log(users);
   res.send(
     `User 1: ${users[0]._id} - ${users[0].username}, User 2: ${users[1]._id} - ${users[1].username}`
   );
